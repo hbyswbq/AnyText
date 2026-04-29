d9cf4e0e7692e3abd95088c1618e0f18ca7cc29d
hbyswbq <w15926777737@gmail.com>
2026-04-29 15:46:56 +0800

    Add files via upload

--- app/src/main/java/com/hhvvg/anytext/hook/AnyHookLoaded.kt
+++ app/src/main/java/com/hhvvg/anytext/hook/AnyHookLoaded.kt
@@ -1,7 +1,7 @@
 package com.hhvvg.anytext.hook
 
 import android.app.Application
-import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -16,40 +16,48 @@ class AnyHookLoaded : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         if (lpparam.packageName == "com.hhvvg.anytext") return
 
+        // 全局 Hook 所有触摸 → 不管什么控件，长按都能取文字
         XposedHelpers.findAndHookMethod(
-            Application::class.java,
-            "onCreate",
+            View::class.java,
+            "dispatchTouchEvent",
+            MotionEvent::class.java,
             object : XC_MethodHook() {
-                override fun afterHookedMethod(param: MethodHookParam) {
-                    val app = param.thisObject as Application
-                    app.registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
-                        override fun onActivityResumed(activity: android.app.Activity) {
-                            hookAllTextViews(activity.window.decorView)
-                        }
-                        override fun onActivityCreated(a: android.app.Activity,b:Bundle?) {}
-                        override fun onActivityStarted(a: android.app.Activity) {}
-                        override fun onActivityPaused(a: android.app.Activity) {}
-                        override fun onActivityStopped(a: android.app.Activity) {}
-                        override fun onActivitySaveInstanceState(a:android.app.Activity,b:Bundle) {}
-                        override fun onActivityDestroyed(a:android.app.Activity) {}
-                    })
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    val event = param.args[0] as MotionEvent
+                    val view = param.thisObject as View
+
+                    if (event.action == MotionEvent.ACTION_DOWN) {
+                        findTextViewAndShowDialog(view, event.rawX, event.rawY)
+                    }
                 }
             }
         )
     }
 
-    private fun hookAllTextViews(root: View) {
-        if (root is ViewGroup) {
-            for (i in 0 until root.childCount) {
-                hookAllTextViews(root.getChildAt(i))
-            }
-        } else if (root is TextView) {
-            root.setOnLongClickListener { v ->
-                TextEditingDialog.show(v.context, root) { newText ->
-                    root.text = newText
+    // 按坐标找最上层的文字（通杀所有界面）
+    private fun findTextViewAndShowDialog(view: View, x: Float, y: Float) {
+        if (view is ViewGroup) {
+            for (i in 0 until view.childCount) {
+                val child = view.getChildAt(i)
+                if (isTouchInView(child, x, y)) {
+                    findTextViewAndShowDialog(child, x, y)
                 }
-                true
+            }
+        } else if (view is TextView && view.isShown) {
+            TextEditingDialog.show(view.context, view) { newText ->
+                view.text = newText
             }
         }
+    }
+
+    // 判断坐标是否在控件上
+    private fun isTouchInView(view: View, x: Float, y: Float): Boolean {
+        val loc = IntArray(2)
+        view.getLocationOnScreen(loc)
+        val left = loc[0]
+        val top = loc[1]
+        val right = left + view.width
+        val bottom = top + view.height
+        return x in left..right && y in top..bottom
     }
 }
