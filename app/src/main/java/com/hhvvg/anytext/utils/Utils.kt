57037e4353171ab9f0e7db46defd9ad01dfca140
hbyswbq <w15926777737@gmail.com>
2026-04-29 14:18:17 +0800

    Add files via upload

--- app/src/main/java/com/hhvvg/anytext/utils/Utils.kt
+++ app/src/main/java/com/hhvvg/anytext/utils/Utils.kt
@@ -1,34 +1,32 @@
 package com.hhvvg.anytext.utils
 
-import android.app.Application
-import android.content.Context
-import android.widget.TextView
 import de.robv.android.xposed.XposedHelpers
+import kotlin.reflect.KClass
 
-fun dp2px(context: Context, dp: Float): Float {
-    val scale = context.resources.displayMetrics.density
-    return dp * scale + 0.5f
-}
+object Utils {
+    // 安全的反射获取字段方法，带类型检查
+    fun <T> getObjectFieldSafe(obj: Any, fieldName: String, clazz: KClass<T>): T? {
+        return try {
+            val value = XposedHelpers.getObjectField(obj, fieldName)
+            if (clazz.isInstance(value)) {
+                @Suppress("UNCHECKED_CAST")
+                value as T
+            } else {
+                null
+            }
+        } catch (e: Exception) {
+            null
+        }
+    }
 
-fun px2dp(context: Context, px: Float): Float {
-    val scale = context.resources.displayMetrics.density
-    return px / scale + 0.5f
-}
-
-fun highlightText(textView: TextView) {
-    appPropertyInject(textView.context.applicationContext as Application, APP_HIGHLIGHT_FIELD_NAME, true)
-    textView.text = textView.text
-}
-
-fun clearTextHighlight(textView: TextView) {
-    appPropertyInject(textView.context.applicationContext as Application, APP_HIGHLIGHT_FIELD_NAME, false)
-    textView.text = textView.text.toString()
-}
-
-fun appPropertyInject(app: Application, fieldName: String, value: Any) {
-    XposedHelpers.setAdditionalInstanceField(app, fieldName, value)
-}
-
-fun <T> getAppInjectedProperty(app: Application, name: String): T {
-    return XposedHelpers.getAdditionalInstanceField(app, name) as T
+    // 如果你之前的代码用了这个方法，保持兼容
+    @Deprecated("Use getObjectFieldSafe instead")
+    fun <T> getObjectField(obj: Any, fieldName: String): T? {
+        return try {
+            @Suppress("UNCHECKED_CAST")
+            XposedHelpers.getObjectField(obj, fieldName) as T?
+        } catch (e: Exception) {
+            null
+        }
+    }
 }
