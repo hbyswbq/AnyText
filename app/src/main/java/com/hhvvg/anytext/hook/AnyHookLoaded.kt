c6d3293321cd01df289c90f234740856513fef93
hbyswbq <w15926777737@gmail.com>
2026-04-29 15:32:34 +0800

    Add files via upload

--- app/src/main/java/com/hhvvg/anytext/hook/AnyHookLoaded.kt
+++ app/src/main/java/com/hhvvg/anytext/hook/AnyHookLoaded.kt
@@ -1,6 +1,9 @@
 package com.hhvvg.anytext.hook
 
+import android.app.Application
+import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
@@ -13,31 +16,40 @@ class AnyHookLoaded : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         if (lpparam.packageName == "com.hhvvg.anytext") return
 
-        // 给所有 TextView 设置长按事件
         XposedHelpers.findAndHookMethod(
-            TextView::class.java,
-            "setOnLongClickListener",
-            View.OnLongClickListener::class.java,
+            Application::class.java,
+            "onCreate",
             object : XC_MethodHook() {
                 override fun afterHookedMethod(param: MethodHookParam) {
-                    val textView = param.thisObject as TextView
-                    hookTextViewLongClick(textView)
+                    val app = param.thisObject as Application
+                    app.registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
+                        override fun onActivityResumed(activity: android.app.Activity) {
+                            hookAllTextViews(activity.window.decorView)
+                        }
+                        override fun onActivityCreated(a: android.app.Activity,b:Bundle?) {}
+                        override fun onActivityStarted(a: android.app.Activity) {}
+                        override fun onActivityPaused(a: android.app.Activity) {}
+                        override fun onActivityStopped(a: android.app.Activity) {}
+                        override fun onActivitySaveInstanceState(a:android.app.Activity,b:Bundle) {}
+                        override fun onActivityDestroyed(a:android.app.Activity) {}
+                    })
                 }
             }
         )
     }
 
-    private fun hookTextViewLongClick(textView: TextView) {
-        // 系统原有长按监听保留
-        val originalListener = textView.onLongClickListener
-
-        textView.setOnLongClickListener { v ->
-            // 先弹我们的框
-            TextEditingDialog.show(v.context, textView) { newText ->
-                textView.text = newText
+    private fun hookAllTextViews(root: View) {
+        if (root is ViewGroup) {
+            for (i in 0 until root.childCount) {
+                hookAllTextViews(root.getChildAt(i))
             }
-            // 再执行原来的长按（不影响APP）
-            originalListener?.onLongClick(v) ?: true
+        } else if (root is TextView) {
+            root.setOnLongClickListener { v ->
+                TextEditingDialog.show(v.context, root) { newText ->
+                    root.text = newText
+                }
+                true
+            }
         }
     }
 }
