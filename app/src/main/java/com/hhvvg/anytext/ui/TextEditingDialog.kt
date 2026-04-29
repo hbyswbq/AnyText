7c937b3a14b2665c54924daa870480443225a094
hbyswbq <w15926777737@gmail.com>
2026-04-29 20:48:26 +0800

    Add files via upload

--- app/src/main/java/com/hhvvg/anytext/ui/TextEditingDialog.kt
+++ app/src/main/java/com/hhvvg/anytext/ui/TextEditingDialog.kt
@@ -7,17 +7,18 @@ import android.app.AlertDialog
 
 object TextEditingDialog {
     fun show(context: Context, textView: TextView, onConfirm: (String) -> Unit) {
-        val edit = EditText(context)
-        edit.setText(textView.text)
-        edit.setSelection(textView.text.length)
+        val editText = EditText(context)
+        editText.setText(textView.text.toString())
 
-        AlertDialog.Builder(context)
-            .setTitle("修改文字")
-            .setView(edit)
-            .setPositiveButton("确定") { _, _ ->
-                onConfirm(edit.text.toString())
-            }
-            .setNegativeButton("取消", null)
-            .show()
+        runCatching {
+            AlertDialog.Builder(context, android.R.style.Theme_DeviceDefault_Dialog)
+                .setTitle("修改文字")
+                .setView(editText)
+                .setPositiveButton("确定") { _, _ ->
+                    onConfirm(editText.text.toString())
+                }
+                .setNegativeButton("取消", null)
+                .show()
+        }
     }
 }
